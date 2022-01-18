bef8e233b9cde249ba08c6f1535a24e942f89d8b
Miss Sylveon <94614122+MissSylveon@users.noreply.github.com>
2022-01-18 22:16:13 +0530

    Update config.py

--- ZeusXRobot/config.py
+++ ZeusXRobot/config.py
@@ -5,63 +5,69 @@ import os
 
 def get_user_list(config, key):
     with open('{}/ZeusXRobot/{}'.format(os.getcwd(), config),
-                  'r') as json_file:
-                          return json.load(json_file)[key]
+              'r') as json_file:
+        return json.load(json_file)[key]
+
 
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
-LOGGER = True
-# REQUIRED
-#Login to https://my.telegram.org and fill in these slots with the details given by it
-API_ID = 4759912  # integer value, dont use 
-API_HASH = "5bcc548f108e30a913a339a4f97f315c"
-TOKEN = "5025924627:AAHoNn9Ht3Zx0ZFPGWlI6mOAx_xcAV-hDsI"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-OWNER_ID = 1323719429  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-OWNER_USERNAME = "Aryanjawale"
-SUPPORT_CHAT = 'trainer_zone'  #Your own group for support, do not add the @
-UPDATES_CHANNEL = 'ZeusUpdates' #Your own channel for Updates of bot, Do not add @
-JOIN_LOGGER = -1001282990849  #Prints any new group the bot is added to, prints just the name and ID.
-REM_BG_API_KEY = "dxsh728mZMDmj4ijSZCNPZig"
-TEMP_DOWNLOAD_DIRECTORY = ""
-EVENT_LOGS = -1001282990849  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:nZ14d0drRRY6Wdz02W8v@containers-us-west-17.railway.app:6926/railway' #do you hub your old heroku app database_URL then put here, most use 25days ago sq
-LOAD = [] #try to kang this db ur big mothersfuckers i know your noob so only kanging my db
-NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
-WEBHOOK = None
-INFOPIC = True
-URL = None
-SPAMWATCH_API = "M7r11AlyDZGvFh2F9U6umCS96iVvyMC~Bo9khw2cKMcYvRBYyDOUNJ7FLQqEEwgP"  # go to support.spamwat.ch to get key
-SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
-BOT_USERNAME = "CatNoir_rbot"
-BOT_ID = "5025924627"
-DRAGONS = get_user_list('elevated_users.json', 'sudos')
-DEV_USERS = get_user_list('elevated_users.json', 'devs')
-##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-DEMONS = get_user_list('elevated_users.json', 'supports')
-#List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-TIGERS = get_user_list('elevated_users.json', 'tigers')
-WOLVES = get_user_list('elevated_users.json', 'whitelists')
-DONATION_LINK = None  # EG, paypal
-CERT_PATH = None
-PORT = 5000
-DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
-STRICT_GBAN = True
-STRICT_GMUTE = True
-WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
-BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-ARQ_API_URL = "https://thearq.tech"
-ARQ_API_KEY = "IZXNVQ-QYIMOM-OVZVEP-PBJNXH-ARQ"
-CASH_API_KEY = 'V7NS1NBFEL4X24L6'  # Get your API key from https://www.alphavantage.co/support/#api-key
-TIME_API_KEY = '2AS711XS1O9B'  # Get your API key from https://timezonedb.com/api
-OPENWEATHERMAP_ID = 'e8c43576833a8867d24a4e6785349e20'
-WALL_API = 'awoo'  #For wallpapers, get one from https://wall.alphacoders.com/api.php
-AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
-BL_CHATS = []  # List of groups that you want blacklisted.
-SPAMMERS = None
+    LOGGER = True
+    # REQUIRED
+    #Login to https://my.telegram.org and fill in these slots with the details given by it
+
+    API_ID = 4759912  # integer value, dont use ""
+    API_HASH = "5bcc548f108e30a913a339a4f97f315c"
+    TOKEN = "5025924627:AAHoNn9Ht3Zx0ZFPGWlI6mOAx_xcAV-hDsI"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1323719429  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Aryanjawale"
+    SUPPORT_CHAT = 'trainer_zone'  #Your own group for support, do not add the @
+    UPDATES_CHANNEL = 'ZeusUpdates' #Your own channel for Updates of bot, Do not add @
+    JOIN_LOGGER = -1001282990849  #Prints any new group the bot is added to, prints just the name and ID.
+    REM_BG_API_KEY = "dxsh728mZMDmj4ijSZCNPZig"
+    TEMP_DOWNLOAD_DIRECTORY = ""
+    EVENT_LOGS = -1001282990849  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:nZ14d0drRRY6Wdz02W8v@containers-us-west-17.railway.app:6926/railway' #do you hub your old heroku app database_URL then put here, most use 25days ago sql
+    LOAD = [] #try to kang this db ur big mothersfuckers i know your noob so only kanging my db
+    NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
+    WEBHOOK = None
+    INFOPIC = True
+    URL = None
+    SPAMWATCH_API = "M7r11AlyDZGvFh2F9U6umCS96iVvyMC~Bo9khw2cKMcYvRBYyDOUNJ7FLQqEEwgP"  # go to support.spamwat.ch to get key
+    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+    BOT_USERNAME = "CatNoir_rbot"
+    BOT_ID = "5025924627"
+    
+    DRAGONS = get_user_list('elevated_users.json', 'sudos')
+
+    DEV_USERS = get_user_list('elevated_users.json', 'devs')
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = get_user_list('elevated_users.json', 'supports')
+    #List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = get_user_list('elevated_users.json', 'tigers')
+    WOLVES = get_user_list('elevated_users.json', 'whitelists')
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    STRICT_GBAN = True
+    STRICT_GMUTE = True
+    WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
+    BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    ARQ_API_URL = "https://thearq.tech"
+    ARQ_API_KEY = "IZXNVQ-QYIMOM-OVZVEP-PBJNXH-ARQ"
+    CASH_API_KEY = 'V7NS1NBFEL4X24L6'  # Get your API key from https://www.alphavantage.co/support/#api-key
+    TIME_API_KEY = '2AS711XS1O9B'  # Get your API key from https://timezonedb.com/api
+    OPENWEATHERMAP_ID = 'awoo'
+    WALL_API = 'e8c43576833a8867d24a4e6785349e20'  #For wallpapers, get one from https://wall.alphacoders.com/api.php
+    AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
+
 
 class Production(Config):
     LOGGER = True
+
 
 class Development(Config):
     LOGGER = True
